b4edb3ca65439282f14b53ce8fb118037f350bd5
David Johansson <david.johansson@dolphinse.com>
2013-06-18 08:28:02 +0200

    Added ilearnrw/prototype/application/program.java (new default prototype application). Renamed project iLearnRW. Added *~ and *.swp to gitignore.

---
+++ src/ilearnrw/prototype/application/program.java
@@ -0,0 +1,13 @@
+package ilearnrw.prototype.application;
+
+public class program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hellow iLearnRW");
+
+	}
+
+}
