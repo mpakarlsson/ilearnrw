10de554b3892f061baecb419a4bee90a4ad52f42
chlitsas <litsasch@gmail.com>
2014-05-05 03:52:07 +0300

    -- old/unused classes removed -- created WordList class and corresponding loader. both support loading a list of words from a file to an arraylist

---
+++ src/ilearnrw/languagetools/extras/WordListLoader.java
@@ -0,0 +1,47 @@
+package ilearnrw.languagetools.extras;
+
+import ilearnrw.resource.ResourceLoader;
+import ilearnrw.resource.ResourceLoader.Type;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class WordListLoader {
+	private InputStream dictionary;
+	private String filename;
+	private WordList words;
+
+	public WordListLoader(String filename) {
+		this.filename = filename;
+		this.words = new WordList();
+		loadFile();
+		readWords();
+	}
+	
+	private void loadFile() {
+		dictionary = ResourceLoader.getInstance().getInputStream(Type.DATA, this.filename);
+	}
+
+	public void readWords(){
+		try {
+			InputStreamReader in = new InputStreamReader(dictionary, "UTF-8");
+			BufferedReader buf = new BufferedReader(in);
+			String line = null;
+			while((line=buf.readLine())!=null) {
+				String[] row = line.split(" ");
+				if (row[0] != null)
+					this.words.add(row[0]);
+			}
+			buf.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public WordList getWordList() {
+		return words;
+	}
+	
+}
