434c7c4f63ba23d8a6f07b2df078702752fbea4f
Chris Litsas <chlitsas@central.ntua.gr>
2014-05-05 13:33:49 +0300

    added exception handling to WordLists class when loading files

--- src/ilearnrw/user/problems/wordlists/ProblemsWordLists.java
+++ src/ilearnrw/user/problems/wordlists/ProblemsWordLists.java
@@ -12,16 +12,19 @@ public class ProblemsWordLists {
 		ProblemDefinitionIndex probs;
 		String path = "greek_collection_for_problems/", lan = "GR";
 		if (lc == LanguageCode.EN){
-			path = "greek_collection_for_problems/";
-			lan = "GR";
+			path = "english_collection_for_problems/";
+			lan = "EN";
 		}
 		probs = new ProblemDefinitionIndex(lc);
 		wordList = new WordList[probs.getIndexLength()][];
 		for (int i=0;i<wordList.length; i++){
 			wordList[i] = new WordList[probs.getRowLength(i)];
 			for (int j=0;j<wordList[i].length; j++){
-				WordListLoader ggl = new WordListLoader(path+"cat"+i+"/words_for_problem_"+i+"_"+j+"_"+lan+".txt");
-				wordList[i][j] = ggl.getWordList();
+				wordList[i][j] = null;
+				try{
+					WordListLoader ggl = new WordListLoader(path+"cat"+i+"/words_for_problem_"+i+"_"+j+"_"+lan+".txt");
+					wordList[i][j] = ggl.getWordList();
+				}catch(Exception e){}
 			}
 		}
 	}
