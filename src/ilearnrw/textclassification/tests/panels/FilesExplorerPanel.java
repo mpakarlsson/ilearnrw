3f55141b8730f91899dc46728a2e0a1c96236dbe
chlitsas <litsasch@gmail.com>
2015-09-23 10:50:32 +0300

    fixed small bug on FilesExplorerPanel class

--- src/ilearnrw/textclassification/tests/panels/FilesExplorerPanel.java
+++ src/ilearnrw/textclassification/tests/panels/FilesExplorerPanel.java
@@ -19,11 +19,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.Map;
 import java.util.Scanner;
 
 import javax.swing.Box;
@@ -235,25 +230,6 @@ public class FilesExplorerPanel extends JPanel{
 			}*/
 		}
 	}
-	
-	private Map<String, Integer> sortByValue(Map<String, Integer> map) {
-		if (map==null)
-			return null;
-		LinkedList<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(map.entrySet());
-
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-
-            public int compare(Map.Entry<String, Integer> m1, Map.Entry<String, Integer> m2) {
-                return (m2.getValue()).compareTo(m1.getValue());
-            }
-        });
-
-        Map<String, Integer> result = new LinkedHashMap<String, Integer>();
-        for (Map.Entry<String, Integer> entry : list) {
-            result.put(entry.getKey(), entry.getValue());
-        }
-        return result;
-    }
 	
 	private class IlearnFile {
 
